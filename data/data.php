886a410c594b71074b8d12bc9c5b9ccf09859da6
findomba <lucyindomba@gmail.com>
2016-05-09 23:44:48 -0400

    base forfait updtae

--- data/data.php
+++ data/data.php
@@ -5,11 +5,10 @@
  * Date: 2016-05-09
  * Time: 22:49
  */
-<?php
+
 /**
  * Tableau principal des données du site
- * Les clefs des items sont leurs "id" (un numéro)
- * (suivant le projet) Chaque item doit contenir au minimum les champs suivants
+    Il y aura 5 types de forfaits par catégorie
  * - nom
  * - categorie
  * - etc
@@ -17,41 +16,57 @@
 
 $data = array(
     0 => array(
-        'nom' => 'Trekking',
+        'nom' => "L'atlantique emblématique",
         'categorie' => 'Croisiere',
+        'description' => "Le produit 1 bla bla bla...",
+        'duree' =>14,
         'prix' => 1899.99,
-        'photo' => 'toto.jpg',
+        'photo' => 'images/images-modal/croisiere_01.jpg',
         'dans_carousel' => true,
+        'date' => 'd-m-y',
     ),
     1 => array(
-        'nom' => 'Robots',
+        'nom' => 'Safari',
         'categorie' => 'Aventure',
+        'description' => "Le produit 1 bla bla bla...",
         'prix' => 489.99,
-        'photo' => 'kiki.jpg',
+        'duree' =>14,
+        'photo' => '',
         'dans_carousel' => false,
+        'date' => 'd-m-y',
     ),
     2 => array(
-        'nom' => 'Football',
+        'nom' => 'Motoneige (Charlevoix, Québec)',
         'categorie' => 'Sport d\'hiver',
+        'description' => "Le produit 1 bla bla bla...",
         'prix' => 88.00,
-        'photo' => 'foot.jpg',
+        'duree' =>10,
+        'photo' => 'images/images-modal/escalade_Husky.jp',
         'dans_carousel' => true,
+        'date' => 'd-m-y',
     ),
 
     3 => array(
-        'nom' => 'Football',
+        'nom' => 'Chiens et gîtes du grand nord',
         'categorie' => 'Nature',
-        'prix' => 88.00,
-        'photo' => 'foot.jpg',
+        'description' => "Le produit 1 bla bla bla...",
+        'prix' => 199.00,
+        'duree' =>3,
+        'photo' => 'images/images-modal/chiens_traineau.jpg',
         'dans_carousel' => true,
+        'date' => 'd-m-y',
     ),
 
     4 => array(
-        'nom' => 'Football',
+        'nom' => 'Formule Hotel tout inclus',
         'categorie' => 'Famille',
-        'prix' => 88.00,
+        'description' => "Le produit 1 bla bla bla...",
+        'prix' => 1600.00,
+        'duree' =>7,
         'photo' => 'foot.jpg',
         'dans_carousel' => true,
+        'date' => 'd-m-y',
+
     ),
 
 
@@ -59,6 +74,8 @@ $data = array(
 
 );
 
+
+//Pour afficher le tableau
 echo '<ul>';
 foreach ($data as $categorie => $nom) {
     echo "<li><a href='$categorie'>$nom</a></li>";
