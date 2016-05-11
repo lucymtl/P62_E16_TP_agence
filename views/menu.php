b8fa19c2d2cf61b9bc43ba8d12f613e56363a2be
findomba <lucyindomba@gmail.com>
2016-05-11 14:31:43 -0400

    correction

--- views/menu.php
+++ views/menu.php
@@ -1,7 +1,12 @@
 
 
 
-        <?php foreach(get_menu() as $menu => $menu_data){
+        <?php
+        require_once('../data/defines.php');
+
+
+
+        foreach(get_menu() as $menu => $menu_data){
             echo '<li class="active"><a href="', $menu_data,'">', $menu,'</a></li>';
         }  ?>
 
