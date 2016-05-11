104c30a54df727fc4118f9989cb9c4027f0f28ab
findomba <lucyindomba@gmail.com>
2016-05-10 23:06:12 -0400

    mise a jour de fonction pour securiser le formulaire

--- utils/utils.php
+++ utils/utils.php
@@ -14,9 +14,26 @@
 </div>
 <!--/Faire une constante pour les liens des images-->
 
-//faire une boucle dan l'index pour appeler les categorie faire afficher le catagories spuis forme de liens
+<!--//faire une boucle dan l'index pour appeler les categorie faire afficher le catagories spuis forme de liens-->
 
-<?php foreach ($categorie as cat_id =>$categorie_){
+<?php foreach ($categories as $cat_id => $nom) { ?>
+    <li><a href="catalogue.php?cat_id=<?= $cat_id ?>"><?= $nom ?></a></li>
+<?php } ?>};
 
 
+<!--Fonction pour securiser le formulaire-->
+<?php
+$nom =$prenom =$pseudo ="";
+function securisation($info){
+($info) = trim($info);
+($info) = stripslashes($info);
+($info) = strip_tags($info);
+return($info);
+
+    prenom = securisation($_POST['prenom']);
+    nom = securisation($_POST['prenom']);
+    pseudo = securisation($_POST['prenom']);
+
+    echo "Bonjour " $pseudo !;
 }
+?>
