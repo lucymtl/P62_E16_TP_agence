648aa8100c2346c10a79d8bcdeae86d77a60a9a4
findomba <lucyindomba@gmail.com>
2016-05-11 22:20:22 -0400

    Le formulaire de loggin

---
+++ register.php
@@ -0,0 +1,49 @@
+
+/**
+ * Created by PhpStorm.
+ * User: findomba
+ * Date: 2016-05-11
+ * Time: 21:50
+ */
+
+
+<?php require_once 'header.php'?>
+<h1>S'incrire</h1>
+<form action="" method="post">
+    <div class="form-group">
+
+        <label for ="">Pseudo</label>
+
+        <input type = "text" name="username" required/>
+
+
+    </div>
+    <div class="form-group" >
+        <label for ="">E-mail</label>
+
+        <input type = "text" name="email" required/>
+
+    </div>
+
+    <div class="form-group" >
+        <label for ="">Mot de passe</label>
+
+        <input type = "password" name="password" required/>
+
+    </div>
+
+
+
+    <div class="form-group" >
+        <label for ="">Confirmez votre mot de passe</label>
+
+        <input type = "password" name="password_confirm" required/>
+
+    </div>
+
+<button type="submit">M'inscrire</button>
+</form>
+
+
+<?php require_once 'footer.php'?>
+
