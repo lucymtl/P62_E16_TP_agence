276102b99737d1add89f83b26bd71b60ab8e35ef
findomba <lucyindomba@gmail.com>
2016-05-11 21:33:48 -0400

    MAJ accueil en fonction de l'ennoncé TP

--- accueil.php
+++ accueil.php
@@ -4,9 +4,31 @@ require_once('data/data.php');//le code va cherche le tableau data
 
 
 
-<?php require_once ('header.php')?>
+<?php //require_once ('header.php')?>
 
 
 </body>
+
+<h1>Bonjour Bienvenue CHEZ AGENCE DE VOYAGE</h1>
+<h2>Consultez notre catalogue</h2>
+
+<?php //require_once ('views/page_top.php'); ?>
+<div id="main">
+    <!-- Liens vers les categories du catalogue -->
+    <div id="liens_categories">
+        <ul>
+            <?php foreach ($categories as $cat_id => $categorie) { ?>
+                <li><a href="catalogue.php?cat_id=<?= $cat_id ?>"><?= $categorie ?></a></li>
+            <?php } ?>
+        </ul>
+
+        <h2>VOTRE PANIER EST VIDE</h2>
+
+    </div>
+    <!--Code html spécifique -->
+</div>
+<?php //require_once ('views/page_bottom.php'); ?>
+
+
 
 </html>
