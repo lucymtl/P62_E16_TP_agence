a566c62ad8cee421c083d4365fde8bdf6c88144f
findomba <lucyindomba@gmail.com>
2016-05-11 21:32:40 -0400

    MAJ catalogue en fonction de l'ennoncé TP

--- catalogue.php
+++ catalogue.php
@@ -1,20 +1,11 @@
-
 <?php
 require_once('data/data.php');
 // Est-ce qu'il y a une categorie (cat_id)  présente dans l'url ?
-$cat_id = null;
+$cat_id = null; // Initialiser u premier des items
 if (array_key_exists('cat_id', $_GET) && array_key_exists($_GET['cat_id'], $categories)) {
     $cat_id = $_GET['cat_id'];
 }
 ?>
-
-
-
-<?php
-foreach ($data as $id => $item) {
-if (is_null($cat_id) || $item['categorie'] == $cat_id) {
-?>
-
 
 
 <!DOCTYPE html>
@@ -23,56 +14,69 @@ if (is_null($cat_id) || $item['categorie'] == $cat_id) {
     <meta charset="UTF-8">
     <title>Title</title>
 <!--    <link href="css/style.css" rel="stylesheet" type="text/css" media="all">-->
+<style>
+    img{
+        width :260px;
+    }
 
-    <style>
+    #toto{
 
-        #ongles{
+        background-color:red ;
+        width: 500px;
 
-            background-color:red;
-        }
+    }
 
+    li{
+        list-style-type:none
+    }
 
-    </style>
+</style>
+
 </head>
+
+
 <body>
-        <div id="body">
-            <?php
-            /*Affichage du catalogue*/
-            // Si il y a une categorie, afficher son nom
-            if ( ! is_null($cat_id)) {
-                echo "<h2>Les items de la catégorie " . $categories[$cat_id] . "</h2>";
+
+<?php //require_once ('views/page_top.php'); ?>
+<div id="main">
+    <!--Code html spécifique -->
+    <h1> AGENCE DE VOYAGE</h1>
+    <h2>Consultez notre catalogue</h2>
+
+    <?php
+    /*Affichage du catalogue*/
+    // Si il y a une categorie, afficher son nom
+    if ( ! is_null($cat_id)) {
+        echo "<h2>Les items de la catégorie " . $categories[$cat_id] . "</h2>";
+    }
+    ?>
+
+    <ul>
+        <?php
+        foreach ($data as $id => $item) {
+            if (is_null($cat_id) || $item['categorie'] == $cat_id) {
+                ?>
+                <li><a href="detail.php?item_id=<?= $id ?>">
+                        <div id = "toto">
+                            <p><?= $item['nom'] ?>
+                                , <span class=".prix"><?= $item['prix'] ?></span>
+                                , <span class=".categorie"><?= $categories[$item['categorie']] ?></span>
+                            </p>
+                            <img src="<?= $item['photo'] ?>" alt=""/>
+                        </div>
+                    </a>
+                </li>
+                <?php
             }
-            ?>
-                        <ul id="ongles">
-                            <?php foreach ($categories as $cat_id => $nom) { ?>
-                                <li><a href="catalogue.php?cat_id=<?= $cat_id ?>"><?= $nom ?></a></li>
-                            <?php } ?>
-                        </ul>
-
-                    <div>
-                        <ul>
-                        <li><?= $item['nom'] ?></li></ul>
-                        , <h3 class=".description"><?= $item['description'] ?></h3>
-                        , <h3 class=".prix"><?= $item['prix'] ?></h3>
-                        , <h4 class=".categorie"><?= $categories[$item['categorie']] ?></h4>
-
-                        <img src="<?= $item['photo'] ?>" alt=""/>
-                    </div>
-                </a>
-
-            <?php
-            }
-            }
-            ?>
-
-                </div>
-
-
-                    </div>
-
-
-
+        }
+        ?>
+    </ul>
+</div>
+<h2>VOTRE PANIER EST VIDE</h2>
+<li><a href="accueil.php"/a>Retour a l'accueil</li>
 
 
 </body>
+
 </html>
+
