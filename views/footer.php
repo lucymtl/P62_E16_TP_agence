b45a13a422503678baba7e2d110b593357156b14
ederml <edermlacerda@gmail.com>
2016-05-15 17:27:56 -0400

    Add files via upload
    Footer

---
+++ views/footer.php
@@ -0,0 +1,6 @@
+<?php
+    $footer = "<p style='text-align: left; color: #33587e; font-size: 12px; padding-top: 5px; font-weight: lighter'>©Copyright - Lucy Indomba | Eder Lacerda</p>";
+       echo "<footer><div id='wrap'>$footer</div></footer>";?>
+
+
+
