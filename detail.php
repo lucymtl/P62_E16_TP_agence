af62a3bf272df626889481dac83dba50f3982784
findomba <lucyindomba@gmail.com>
2016-05-14 23:45:26 -0400

    maj du define pour le menu

--- detail.php
+++ detail.php
@@ -23,27 +23,28 @@ $categories = $data[$cat_id];
 
 
 
-
+******code prof****
 <?php
-//    var_dump($_GET);
-$cat_id = 0; // Initialiser au premier des items
-if (array_key_exists('item_id', $_GET)) {
-    $categories_id = $_GET['item_id'];
-}
-// Il faut vérifier que la valeur de l'id est bonne
-$cat_id = get_truc($categories_id);
-?>
-<?php //require_once ('views/page_top.php'); ?>
-<div id="main">
-    <?php if (is_null($categories)) { ?>
-        <p>Ce truc n'existe pas !</p>
-    <?php } else { ?>
-        <div>
-            <p><?= $categories['name'] ?>, <span class=".prix"><?= $categories['price'] ?></span></p>
-            <img src="<?= IMAGE_PATH . '/' . $categories['picture'] ?>" alt=""/>
-        </div>
-    <?php } ?>
-    <!--Code html spécifique -->
-</div>
-<?php require_once ('views/page_bottom.php'); ?>
 
+////    var_dump($_GET);
+//$cat_id = 0; // Initialiser au premier des items
+//if (array_key_exists('item_id', $_GET)) {
+//    $categories_id = $_GET['item_id'];
+//}
+//// Il faut vérifier que la valeur de l'id est bonne
+//$cat_id = get_truc($categories_id);
+//?>
+<?php ////require_once ('views/page_top.php'); ?>
+<!--<div id="main">-->
+<!--    --><?php //if (is_null($categories)) { ?>
+<!--        <p>Ce truc n'existe pas !</p>-->
+<!--    --><?php //} else { ?>
+<!--        <div>-->
+<!--            <p>--><?//= $categories['name'] ?><!--, <span class=".prix">--><?//= $categories['price'] ?><!--</span></p>-->
+<!--            <img src="--><?//= IMAGE_PATH . '/' . $categories['picture'] ?><!--" alt=""/>-->
+<!--        </div>-->
+<!--    --><?php //} ?>
+<!--    <!--Code html spécifique -->-->
+<!--</div>-->
+<?php //require_once ('views/page_bottom.php'); ?>
+<!---->
