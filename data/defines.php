c542d9830f0d6659fc180df70b23a8a87d6b7494
findomba <lucyindomba@gmail.com>
2016-05-14 21:18:53 -0400

    maj du define pour le menu

--- data/defines.php
+++ data/defines.php
@@ -1,15 +1,19 @@
 <?php
 
-define('ACCUEIL', 'Accueil');
-define('APROPOS', 'À propos');
+define('ACCUEIL', 'accueil');
+define('CATALOGUE', 'catalogue');
+define('COMMANDE', 'commande');
+define('QUITTER', 'quitter');
+
+
+
 define('CONTACT', 'Contact');
 
 $menu = array(
-    ACCUEIL => 'accueil.php',
-    APROPOS  => 'apropos.php',
-    /*'Catalogue' => 'catalogue.php',*/
-    CONTACT  => 'formulaire.php',
-    CONTACT  => 'formulaire.php',
+    ACCUEIL => 'index.php',
+    CATALOGUE  => 'catalogue.php',
+    COMMANDE => 'commande.php',
+    QUITTER => 'quitter.php',
 
 );
 ?>
