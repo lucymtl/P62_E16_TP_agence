b06c78af3735eb9e531f3ab012549f39ace9ba0b
findomba <lucyindomba@gmail.com>
2016-05-14 21:26:49 -0400

    creation logg in/mag page bottom et top

---
+++ views/page_bottom.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: findomba
+ * Date: 2016-05-14
+ * Time: 20:30
+ */
+?>
+<?php
+?>
+
+</div>
+</body>
+</html>
---
+++ views/page_top.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: findomba
+ * Date: 2016-05-14
+ * Time: 20:24
+ */
+?>
+<?php
+?>
+    <!DOCTYPE html>
+    <html>
+    <head lang="en">
+        <meta charset="UTF-8">
+        <title>AGENCE DE VOYAGE</title>
+    </head>
+    <body>
+    <div id="wrapper">
+<?php //require_once ('header.php'); ?>
