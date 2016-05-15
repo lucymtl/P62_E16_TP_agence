c26b39da50bf8c0b5d337d4cd6eacf3957c795f7
findomba <lucyindomba@gmail.com>
2016-05-14 21:27:34 -0400

    maj de la page acceuil

--- index.php
+++ index.php
@@ -1,32 +1,35 @@
 <?php
 require_once('data/data.php');//le code va cherche le tableau data
-require_once('utils/loggin_out.php');
+require_once('utils/login_out.php');
 ?>
 
 
 
-<?php //require_once ('header.php')?>
+<?php require_once ('views/header.php')?>
 
 
 
 
-<h1>Bonjour Bienvenue CHEZ AGENCE DE VOYAGE</h1>
-<h2>Consultez notre catalogue</h2>
 
-<?php //require_once ('views/page_top.php'); ?>
+
+
+<?php require_once ('views/page_top.php'); ?>
 <div id="main">
     <!-- Liens vers les categories du catalogue -->
-    <div id="liens_categories">
-        <ul>
-            <?php foreach ($categories as $cat_id => $categorie) { ?>
-                <li><a href="catalogue.php?cat_id=<?= $cat_id ?>"><?= $categorie ?></a></li>
-            <?php } ?>
-        </ul>
+
+
+<!--    <div id="liens_categories">-->
+<!--        <ul>-->
+<!--            --><?php //foreach ($categories as $cat_id => $categorie) { ?>
+<!--                <li><a href="catalogue.php?cat_id=--><?//= $cat_id ?><!--">--><?//= $categorie ?><!--</a></li>-->
+<!--            --><?php //} ?>
+<!--        </ul>-->
 
         <h2>VOTRE PANIER EST VIDE</h2>
+        <?php require_once ('views/login_out_form.php'); ?>
 
     </div>
     <!--Code html spécifique -->
 </div>
-<?php //require_once ('views/page_bottom.php'); ?>
+<?php require_once ('views/page_bottom.php'); ?>
 
