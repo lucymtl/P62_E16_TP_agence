34feb4221f3d69168f1e735c1d67b04f8837bc17
findomba <lucyindomba@gmail.com>
2016-05-16 13:27:44 -0400

    integration fichier prof en cours ...

---
+++ db/defines_prod.php
@@ -0,0 +1,6 @@
+<?php
+// Constantes rassemblant les infos de connexion et de schéma de la DB
+define('CONN_HOST', 'localhost');
+define('CONN_USER', 'proto-db-user');
+define('CONN_PWD', 'a7uhuhune');
+define('DBNAME', 'p62e16gp_proto');
