cf5e32e03a94e7f373be9c6da0e2ea6e17fd38e6
ederml <edermlacerda@gmail.com>
2016-05-16 02:32:37 -0400

    Add files via upload

--- data/defines.php
+++ data/defines.php
@@ -2,15 +2,27 @@
 
 define('ACCUEIL', 'Accueil');
 define('APROPOS', 'À propos');
-define('CONTACT', 'Contact');
+define('RESERVER', 'Reserver');
 
 $menu = array(
     ACCUEIL => 'accueil.php',
     APROPOS  => 'apropos.php',
-    /*'Catalogue' => 'catalogue.php',*/
-    CONTACT  => 'contact.php',
-    lero  => 'formulaire.php',
+   
+    RESERVER  => 'contact.php', /*'Catalogue' => 'catalogue.php',*/
+
 
 );
 ?>
-<!--//Faire un define pour le photos et inclure dans data pour appel-->
+<?php
+define('F_FORFAITS', 'forfaits');
+define('P_ACTION', 'action');
+define('OP_AJOUTER', 'OP_AJOUTER');
+define('OP_RETIRER', 'OP_RETIRER');
+define('OP_RETIRER_TOUT', 'OP_RETIRER_TOUT');
+define('OP_VIDER_PANIER', 'OP_VIDER_PANIER');
+
+define('FORFAIT_CROISIERE', 'FORFAIT_CROISIERE');
+define('FORFAIT_NATURE', 'FORFAIT_NATURE');
+define('FORFAIT_SPORTHIVER', 'FORFAIT_SPORTHIVER');
+?>
+
