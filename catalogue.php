57112f6707007b01f614d130b4dbeaa0f4aa71e7
findomba <lucyindomba@gmail.com>
2016-05-18 00:06:58 -0400

    progression de l'affichage des catégories sur le page catalogue.php

--- catalogue.php
+++ catalogue.php
@@ -1,12 +1,14 @@
 <?php
 require_once('defines.php');
 require_once('db/conn.php');
+require_once('utils/login_out.php');
+require_once('utils/panier.php');
 
 $categories = get_categories();
 
 //var_dump($categories);
 // Est-ce qu'il y a une categorie (cat_id)  présente dans l'url ?
-$cat_id = null; // Initialiser u premier des items
+$cat_id = false; // Initialiser u premier des items
 if (array_key_exists('item_id', $_GET) && array_key_exists($_GET['item_id'], $categories)) {
     $cat_id = $_GET['item_id'];
 }
@@ -50,25 +52,42 @@ $forfaits = get_article_list($cat_id);
     <h1> AGENCE DE VOYAGE</h1>
     <h2>Consultez notre catalogue</h2>
 
+<!--    --><?php
+//    /*Affichage du catalogue*/
+//    // Si il y a une categorie, afficher son nom
+//    if ( ! is_null($cat_id)) {
+//        echo "<h2>Les items de la catégorie " . $categories['item_id'] . "</h2>";
+//    }
+//    ?>
+
     <?php
-    /*Affichage du catalogue*/
     // Si il y a une categorie, afficher son nom
-    if ( ! is_null($cat_id)) {
-        echo "<h2>Les items de la catégorie " . $categories['item_id'] . "</h2>";
+    if (false !== $cat_id) {
+        echo "<h2>Les items de la catégorie " . $categories[$cat_id]['name'] . "</h2>";
     }
     ?>
 
+    <?php
+    /* Affichage du catalogue */
+    if (empty($categories)) {
+        echo "<p>Cette catégorie est vide !.</p>";
+    } else {
+    ?>
+
+
+
+
     <ul>
         <?php
-        foreach ($forfait as $id => $item) {
+        foreach ($categories as $id => $forfait) {
         ?>
                 <li><a href="detail.php?item_id=<?= $id ?>">
                         <div id = "toto">
-                            <p><?= utf8_encode($item['name']) ?>
-                                , <span class=".prix"><?= $item['price'] ?></span>
-                                , <span class=".categorie"><?= utf8_encode($categories[$item['category_id']]['name']) ?></span>
+                            <p><?= utf8_encode($forfait['name']) ?>
+                                , <span class=".prix"><?= $forfait['price'] ?></span>
+                                , <span class=".categorie"><?= utf8_encode($categories[$forfait['category_id']]['name']) ?></span>
                             </p>
-                            <img src="<?= $item['picture'] ?>" alt=""/>
+                            <img src="<?= $forfait['picture'] ?>" alt=""/>
                         </div>
                     </a>
                 </li>
@@ -77,12 +96,12 @@ $forfaits = get_article_list($cat_id);
         ?>
     </ul>
 </div>
-<h2>VOTRE PANIER EST VIDE</h2>
-<li><a href="accueil.php"/a>Retour a l'accueil</li>
-
-
-
-</body>
-
-</html>
+<!--<h2>VOTRE PANIER EST VIDE</h2>-->
+<!--<li><a href="accueil.php"/a>Retour a l'accueil</li>-->
+<!---->
+<!---->
+<!---->
+<!--</body>-->
+<!---->
+<!--</html>-->
 
