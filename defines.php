fa949975a819e0ebbb2f3bb5097e20ca107e7cc1
findomba <lucyindomba@gmail.com>
2016-05-17 21:09:31 -0400

    modif en cours

--- defines.php
+++ defines.php
@@ -3,6 +3,7 @@
  * Constantes générales du site
  *
  */
-define ('IMAGE_PATH' , 'image'); // Path relatif des images
+define ('IMAGE_PATH' , 'images'); // Path relatif des images
 define('ROOT_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 define('SITE_NAME', 'P62_site_prototype');
+?>
--- detail.php
+++ detail.php
@@ -2,6 +2,7 @@
 require_once('top.php');//le code va cherche le tableau data
 require_once('defines.php');
 require_once('db/conn.php');
+require_once('utils/panier.php');
 
 //    var_dump($_GET);
 $id_forfait = 0; // Initialiser au premier des items
@@ -22,19 +23,23 @@ $categories = get_categories();
             <div>
                 <div class="acheter"><?= $forfait['name'] ?> - <?= $forfait['price'] ?></div>
                 <div class="button_choix">
-                    <ul>
-                        <li class="button_choix_txt">Il a <span
-                                class="button_choix_valeur"><?= $_SESSION[$id_forfait] ?></span> forfaits dans le
-                            panier
-                        </li>
-                        <li><a href="<?php echo make_action_url(OP_RETIRER_TOUT, $forfait); ?>"><img
-                                    src="images/suprimer.jpg"></a></li>
-                    </ul>
+<!--                    <ul>-->
+<!--                        <li class="button_choix_txt">Il a <span-->
+<!--                                class="button_choix_valeur">--><?//= $_SESSION[CONN_USER] ?><!--</span> forfaits dans le-->
+<!--                            panier-->
+<!--                        </li>-->
+<!--                        <li><a href="--><?php //echo make_action_url(OP_RETIRER_TOUT, $forfait); ?><!--"><img-->
+<!--                                    src="images/suprimer.jpg"></a></li>-->
+<!--                    </ul>-->
+
+                    <div id="card_handling"><input type="number" class="card_item_qty" data-card-item-id="<?= $forfait ?>" value="<?= array_key_exists( $_GET['item_id'], $panier) ? $panier['item_id'][PSESS_CARD_QTY] : 0?>" min="0"/></div>
+                    <img src="<?= IMAGE_PATH . '/' . $forfait['picture'] ?>" alt=""/>
+
                 </div>
-                <img id="price" width="930px" src="<?= $id_forfait['picture'] ?>" alt=""/>
-                <div class="txt_forfait"><p><?= $id_forfait['description'] ?></p></div>
-                <div class="button_reserver"><a href="<?php echo make_action_url(OP_AJOUTER, $id_forfait); ?>">Ajouter
-                        au panier</a></div>
+<!--                <img id="price" width="930px" src="--><?//= $id_forfait['picture'] ?><!--" alt=""/>-->
+                <div class="txt_forfait"><p><?= $forfait['description'] ?></p></div>
+<!--                <div class="button_reserver"><a href="--><?php //echo make_action_url(OP_AJOUTER, $id_forfait); ?><!--">Ajouter-->
+<!--                        au panier</a></div>-->
                 <br/>
             </div>
         <?php } ?>
