e15229dbbd7129e5c105dc69c085c4e7a2f82a32
findomba <lucyindomba@gmail.com>
2016-05-18 11:46:53 -0400

    L'affichage est ok ne pas toucher.

--- detail.php
+++ detail.php
@@ -1,14 +1,12 @@
 <?php
-require_once('top.php');//le code va cherche le tableau data
 require_once('defines.php');
 require_once('db/conn.php');
-require_once('catalogue.php');
-
+//require_once('catalogue.php');
 require_once('utils/panier.php');
 require_once('utils/login_out.php');
 
 
-//    var_dump($_GET);
+    var_dump($_GET);
 $id_forfait = 0; // Initialiser au premier des items
 if (array_key_exists('item_id', $_GET)) {
     $id_forfait = $_GET['item_id'];
@@ -16,19 +14,21 @@ if (array_key_exists('item_id', $_GET)) {
 // Il faut vérifier que la valeur de l'id est bonne
 $forfait = get_article($id_forfait);
 
-$categories = get_categories();
+//$categories = get_categories();
 
 ?>
+<?php require_once('top.php');?>
 <div id="wrap">
 
 
 
     <div id="panier">
         <?php if (is_null($forfait)) { ?>
-            <p>Cet article n'existe pas !</p>
+            <p>Ce forfait  n'existe pas !</p>
         <?php } else { ?>
             <div>
-                <div class="acheter"><?= $forfait['name'] ?> - <?= $forfait['price'] ?></div>
+                <div class="acheter"><?= $forfait['name'] ?> - <?= $forfait['price']."$" ?></div>
+                <div class="acheter"><?= $forfait['duree'].'jours' ?></div>
                 <div class="button_choix">
 <!--                    <ul>-->
 <!--                        <li class="button_choix_txt">Il a <span-->
@@ -39,8 +39,8 @@ $categories = get_categories();
 <!--                                    src="images/suprimer.jpg"></a></li>-->
 <!--                    </ul>-->
 
-                    <div id="card_handling"><input type="number" class="card_item_qty" data-card-item-id="<?= $forfait ?>" value="<?= array_key_exists( $_GET['item_id'], $panier) ? $panier['item_id'][PSESS_CARD_QTY] : 0?>" min="0"/></div>
-                    <img src="<?= IMAGE_PATH . '/' . $forfait['picture'] ?>" alt=""/>
+                    <div id="card_handling"><input type="number" class="card_item_qty" data-card-item-id="<?= $id_forfait ?>" value="<?= array_key_exists( $id_forfait, $panier) ? $panier[$id_forfait][PSESS_CARD_QTY] : 0?>" min="0"/></div>
+                    <img src="<?=  $forfait['picture'] ?>" alt=""/>
 
                 </div>
 <!--                <img id="price" width="930px" src="--><?//= $id_forfait['picture'] ?><!--" alt=""/>-->
@@ -52,7 +52,7 @@ $categories = get_categories();
         <?php } ?>
 
     </div>
-</div><?php require_once('views/video.php'); ?>
+<!--</div>--><?php //require_once('views/video.php'); ?>
 <?php require_once('views/forfaits.php'); ?>
 <?php //require_once('views/catalogue.php'); ?><!--</div>-->
 
