23600bb903e5d3f119b1fcabac35340787e5e90a
findomba <lucyindomba@gmail.com>
2016-05-18 00:08:48 -0400

    probleme affichae des catégories résolu.

---
+++ accueil.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once('defines.php');
+require_once('db/conn.php');
+require_once('utils/login_out.php');
+require_once('utils/panier.php');
+$categories = get_categories();
+//var_dump($categories);
+
+?>
+<?php require_once ('top.php'); ?>
+
+<h2>Consultez notre catalogue</h2>
+
+<div id="main">
+    <!-- Liens vers les categories du catalogue -->
+    <div id="liens_categories">
+        <ul>
+            <?php foreach ($categories as $cat) { ?>
+                <li><a href="catalogue.php?cat_id=<?= $cat['id'] ?>"><?= $cat['name'] ?></a></li>
+            <?php } ?>
+        </ul>
+
+    </div>
+
+    </div>
+    <!--Code html spécifique -->
+</div>
+<?php //require_once ('views/page_bottom.php'); ?>
+
+
+
+</html>
